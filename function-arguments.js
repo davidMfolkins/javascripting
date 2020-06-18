5ffefa1e4c83d39a7448b14792389d9e76faa89e
David Folkins <david.folkins88@gmail.com>
2020-06-18 14:04:59 +0000

    Initial commit with all my work so far

---
+++ function-arguments.js
@@ -0,0 +1,5 @@
+function math(bing, bang, boom) {
+  return bang * boom + bing
+}
+
+console.log(math(53, 61, 67))
---
+++ rounding-numbers.js
@@ -0,0 +1,7 @@
+const roundUp = 1.5
+
+Math.round()
+
+const rounded = Math.round(roundUp)
+
+console.log(rounded)
